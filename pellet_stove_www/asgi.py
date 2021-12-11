85b3bfdff844a3fa3856996651a425dad3b26da7
gaga <gaga@gaga.com>
2021-12-11 14:46:31 +0100

    add django files (command 'django-admin startproject pellet_stove_www .')

---
+++ pellet_stove_www/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pellet_stove_www project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pellet_stove_www.settings')
+
+application = get_asgi_application()
