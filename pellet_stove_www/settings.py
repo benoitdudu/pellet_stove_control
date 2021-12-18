99192701af661ce3bbbc0017a74ee146e9536f2d
Benoit Dudu <gaga@gaga.com>
2021-12-18 21:33:10 +0100

    close the file when it is not used anymore

--- pellet_stove_www/settings.py
+++ pellet_stove_www/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 f = open(BASE_DIR + '/.env.json')
 DATA_ENV = json.load(f)
+f.close
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
